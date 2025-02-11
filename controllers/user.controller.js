0ed96817248bd0b04a6826691883910fa33850ed
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 18:51:43 +0545

    some fixes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,7 @@
-import { getUserService } from "../services/user.service.js";
+import {
+  getUserByIdService,
+  getUserService,
+} from "../services/user.service.js";
 
 export const getUsers = async (req, res, next) => {
   try {
@@ -7,6 +10,20 @@ export const getUsers = async (req, res, next) => {
       success: true,
       data: users,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const paramId = req.params.id;
+    console.log(paramId);
+    const { user } = await getUserByIdService(paramId);
+    if (!user) {
+      res.status(404).json({ success: false, message: "User not found." });
+    }
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
     next(error);
   }
