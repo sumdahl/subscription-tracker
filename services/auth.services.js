122a179018887054e5647e88e942e624d3f72b9e
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 12:35:19 +0545

    create an auth service , <-> communicate with DB

---
+++ services/auth.services.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+
+export const createUser = async (username, email, password) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error = new Error("User already exists.");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create(
+      [{ username, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+    await session.commitTransaction();
+    session.endSession();
+    return { user: newUsers[0], token };
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
+};
