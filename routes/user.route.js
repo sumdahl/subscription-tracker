1ff0cd2169e62986426973d8c992057712b4bbdb
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 13:20:57 +0545

    add route for get all users

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { getUsers } from "../controllers/user.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "Get all users" }));
+userRouter.get("/", authenticateToken, getUsers);
 userRouter.get("/:id", (req, res) => res.send({ title: "READ specific user" }));
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new users" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE new users" }));
