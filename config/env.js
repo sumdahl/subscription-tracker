3a067526a08cc1452e0fb88d89158e5dd573a88f
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 18:50:54 +0545

    add ARCJET environment

--- config/env.js
+++ config/env.js
@@ -2,5 +2,15 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
