2c66c4bb65ecb5ce3d74b93228417802476715f7
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 21:59:11 +0545

    re arrange the order of arcjet middleware, lmao wtf is this

--- app.js
+++ app.js
@@ -14,17 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
-app.use(arcjetMiddleware);
-
-app.get("/", (_, res) => {
-  res.send("Welcome to the world");
-});
 
 app.listen(PORT, async () => {
   console.log(
