655b45f2903ef3c74d1502698327a6a04803757f
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-02-11 12:34:44 +0545

    inject some middlewares

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import subscriptionRouter from "./routes/subscription.route.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (_, res) => {
   res.send("Welcome to the world");
